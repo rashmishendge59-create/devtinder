24be21e614a1b037fa3b1ae112042b510ab55012
rashmisumhr <69501895+rashmisumhr@users.noreply.github.com>
2026-01-16 21:03:50 +0530

    playing with routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
 
-
-app.use('/test',(req,res) => {
-res.send("hello from test")
-}); 
-
-app.use((req,res) => {
-res.send("hello from ")
-});
+app.use("/user",
+    (req,res,next) => {
+        console.log("1 rh ");
+        next();
+    },
+    (req,res,next) =>{
+        console.log("2 rh");
+        next();
+    },
+    (req,res,next) => {
+        console.log("3rd rh");
+        res.send("3rd rh");
+        next();
+    }
+);
 
 
 
